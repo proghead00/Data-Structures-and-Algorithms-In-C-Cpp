e66168ea73cf645adb80d66e7a0e66b21e77893b
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2020-12-28 20:10:05 +0530

    Add files via upload

---
+++ palindromeString.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main()
+{
+
+    char a[] = "madam";
+    char b[6];
+
+    int i, j;
+
+    // reversing the string
+    for (i = 0; a[i] != '\0'; i++)
+    {
+    }
+    i--;
+    for (j = 0; i >= 0; i--, j++)
+    {
+
+        b[j] = a[i];
+    }
+    b[j] = '\0';
+
+    // comparing the strings
+    for (i = 0, j = 0; a[i] != '\0' || b[j] != '\0'; i++, j++)
+    {
+
+        if (a[i] != b[j])
+            break;
+    }
+
+    if (a[i] == b[j])
+        printf("Palindrome string \n");
+
+    else
+        printf("Not a palindrome string \n");
+}
