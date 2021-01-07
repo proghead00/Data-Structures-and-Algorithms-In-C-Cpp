2922330c6ce166b41ca71481a4f318d93fd81ae1
proghead00 <susnatadj.rocks@gmail.com>
2021-01-07 21:28:26 +0530

    Counting no. of nodes and sum in a LL with Recursion

---
+++ Linked List/countAndSum.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+} *first = NULL;
+
+void create(int A[], int n)
+{
+    int i;
+    struct Node *t, *last;
+    first = (struct Node *)malloc(sizeof(struct Node));
+    first->data = A[0];
+    first->next = NULL;
+    last = first;
+
+    for (i = 1; i < n; i++)
+    {
+        t = (struct Node *)malloc(sizeof(struct Node));
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+
+int Rcount(struct Node *p)
+{
+    if (p != NULL)
+        return Rcount(p->next) + 1;
+    else
+        return 0;
+}
+
+int Rsum(struct Node *p)
+{
+    if (p == NULL)
+        return 0;
+    else
+        return Rsum(p->next) + p->data;
+}
+
+int main()
+{
+    int A[] = {1, 1, 1, 1, 1, 1, 1, 1};
+    create(A, 8);
+
+    printf("Count %d\n", Rcount(first));
+    printf("Sum %d\n", Rsum(first));
+
+    return 0;
+}
